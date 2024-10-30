afd86fd428b2bd3239a24c518cc32e70eeeeb8db
willock0 <148595929+willock0@users.noreply.github.com>
2024-10-30 18:57:13 -0300

    Atualizado com vidas e texturas

---
+++ Detona Ralph/Detona Ralph/audios/scripts/script.js
@@ -0,0 +1,85 @@
+
+
+
+
+const state = {
+    viem: {
+        squares: document.querySelectorAll(".square"),
+        enemy : document.querySelector('.enemy'),
+        time : document.querySelector('#time'),
+        score : document.querySelector('#score'), 
+        lives : document.querySelector('#lives'),
+    },
+
+    values:{
+        vidas: 3,
+        timerId : null,
+      
+        tempoC : 60,
+        hitPosition: 0,
+        result : 0,
+    },
+    actions : {
+          countDownTimerid : setInterval(countDown,1000),
+    }
+}
+
+function randomSquare(){
+    state.viem.squares.forEach((square)=> {
+        square.classList.remove('enemy');
+    })
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.viem.squares[randomNumber];
+    randomSquare.classList.add('enemy')
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+    state.viem.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+
+    if(square.id === state.values.hitPosition){
+        state.values.result++
+        state.viem.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+        playSound();
+    }else{
+        state.values.vidas--;
+        state.viem.lives.textContent = state.values.vidas;
+        if(state.values.vidas<= 0){
+            alert(`Fim de jogo sua pontuação foi ${state.values.result}`)
+            location.reload()
+        }
+    }
+
+   });
+});
+}
+
+function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare,1000)
+}
+
+function countDown(){
+    state.values.tempoC--;
+    state.viem.time.textContent = state.values.tempoC;
+    if(state.values.tempoC < 0){
+        clearInterval(state.actions.countDownTimerid);
+        clearInterval(state.values.countDown);
+        alert(`Game over sua pontuação foi ${state.values.result}`)
+        location.reload()
+    }
+}
+
+function playSound() {
+    let audio = new Audio('./hit.m4a');
+    audio.play().catch(error => {
+        console.log("Erro ao reproduzir o áudio:", error);
+    });
+}
+
+function init(){
+ moveEnemy()
+ addListenerHitBox()
+}
+init();
